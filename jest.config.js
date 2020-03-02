83808b3a619dd7fd12891dda0c9857b5fa5e51fa
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-03-02 17:25:24 +0700

    add project source

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,27 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^database/(.*)$': '<rootDir>/src/database/$1',
+    '^schema': '<rootDir>/src/schema',
+    '^controllers': '<rootDir>/src/controllers',
+    '^services': '<rootDir>/src/services',
+    '^repositories': '<rootDir>/src/repositories',
+    '^pubsub/(.*)$': '<rootDir>/src/pubsub/$1',
+    '^helpers/(.*)$': '<rootDir>/src/helpers/$1',
+  },
   setupFilesAfterEnv: ['./jest.setup.js'],
-  testEnvironment: "node"
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!<rootDir>/src/controllers/internal/*.ts",
+    "!<rootDir>/src/services/internal/*.ts",
+    "!<rootDir>/src/database/**/*.ts",
+    "!<rootDir>/src/utils/**/*.ts",
+    "!<rootDir>/src/app/**/*.ts",
+    "!<rootDir>/src/index.ts",
+    "!<rootDir>/src/pubsub/**/*.ts",
+  ]
 }
