0eb65cbadc4e5da745ba59995d9ba6c8fc29a97b
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-03-07 10:35:51 +0700

    handle getConnection automatically

--- src/repositories/repository.ts
+++ src/repositories/repository.ts
@@ -1,8 +1,9 @@
-import { injectable, inject, interfaces } from 'inversify'
+import { injectable, inject, interfaces, } from 'inversify'
 import { DBClient } from 'database/connection.database'
+import { getRepository, EntitySchema } from 'typeorm'
 
 @injectable()
-export class Repository {
+export class Repository<T> {
   private provider: interfaces.Provider<DBClient>
 
   constructor(
@@ -11,8 +12,12 @@ export class Repository {
     this.provider = provider
   }
 
-  public async getConnection() {
-    const connection = await this.provider()
-    return Promise.resolve(connection)
+  private async getConnection(): Promise<DBClient | ((...args: any[]) => Promise<DBClient>)> {
+    return Promise.resolve(await this.provider())
+  }
+
+  public async getRepository(entity: { (): T } | EntitySchema<T> | string | Function) {
+    await this.getConnection()
+    return getRepository<T>(entity)
   }
 }
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,12 +1,10 @@
-import { getRepository } from 'typeorm'
 import { UserEntity } from 'database/entities'
 import { injectable } from 'inversify'
 import { Repository } from './repository'
 
 @injectable()
-export class UserRepository extends Repository {
+export class UserRepository extends Repository<UserEntity> {
   public async getAll() {
-    await this.getConnection()
-    return getRepository(UserEntity).find()
+    return (await this.getRepository(UserEntity)).find()
   }
 }
